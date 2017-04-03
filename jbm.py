491c3c2b5c484b2649ba4da804c26db3c9cb7ef3
bg <bg@example.com>
2017-04-03 16:45:18 +0800

    增加下载最近第3季度报表

--- jbm.py
+++ jbm.py
@@ -70,7 +70,7 @@ class jbm:
         ybsUrl = selector.xpath('//div[@class = "report"]/a')
 
         #print(ybsUrl)
-        for yburl in ybsUrl:
+        for yburl in ybsUrl:    # todo 增加研报日期
 
             ybHtml = requests.get(yburl.xpath('./@href')[0], headers=headers)
             ybHtml.encoding = 'utf-8'
@@ -138,3 +138,48 @@ class jbm:
                     continue
         except:
             print('年报列表页面编码错误;' + url)
+
+    def zq3jbNearest(self):
+        stock = self.__code
+        headers = {'content-type': 'application/json',
+                   'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:22.0) Gecko/20100101 Firefox/22.0'}
+
+        url = 'http://vip.stock.finance.sina.com.cn/corp/go.php/vCB_BulletinSan/stockid/' + stock + '/page_type/sjdbg.phtml'
+        html = requests.get(url, headers=headers)
+        html.encoding = 'utf-8'
+        # req.add_header('User-Agent','Mozilla/5.0 (Windows NT 6.2; rv:16.0) Gecko/20100101 Firefox/16.0')
+
+        selector = etree.HTML(html.content)
+        name = pdStock.loc[stock]['name']
+        # print(url)
+        try:
+            jd3b = (selector.xpath('//div[@class="datelist"]/ul/a')[0])
+            # print(nbs)
+            dirName = stock + name
+            try:
+                os.mkdir('./' + dirName)
+            except:
+                pass
+
+
+            target_url = 'http://vip.stock.finance.sina.com.cn' + jd3b.xpath('./@href')[0]
+            # print(target_url)
+
+            fileHtml = requests.get(target_url, headers=headers)
+            fileHtml.encoding = 'utf-8'
+            fSelector = etree.HTML(fileHtml.content)
+            # print(fileHtml.content)
+            try:
+                fileUrl = fSelector.xpath('//a[contains(text(),"查看PDF公告")]/@href')[0]
+                dataPub = fSelector.xpath('//td[contains(text(),"公告日期")]/text()')[0]
+
+                fileName = (name + str(int(dataPub.split(':')[1].split('-')[0])) + '年3季报')
+
+                local = './' + dirName + '/' + fileName + '.pdf'
+                if not os.path.isfile(local):
+                    # print(local)
+                    urllib.request.urlretrieve(fileUrl, local, None)
+            except:
+                print('PDF失效;' + fileUrl)
+        except:
+            print('3季度报列表页面编码错误;' + url)
